da5c6c194d0707b75ad0ea5b7fcbfd05574ad783
Maxim Podbolotov <podbolotov.maxim@gmail.com>
2025-03-01 14:41:28 +0500

    intermediate_commit;

--- content/main_page.py
+++ content/main_page.py
@@ -1,46 +1,117 @@
 from core.vars import ServiceVariables
 from content.core_page_template import get_core_page_template
 
+
 def get_main_page_content(quote, head_chance, tail_chance, history):
-    main_page = f"""
-    <center>
-    <h1>Честная монетка</h1>
-    <h3>{quote}</h3>
-    <hr>
-    <p>Если тебе выпала не та сторона, которую ты хотел - шансы изменятся в твою пользу.</p>
-    <h1 id="waiting-text" class="status-label">Ожидание броска</b></h1>
-    <p><b>Текущие шансы</b></p>
-    <table class="table">
-    <thead>
-      <tr>
-        <th>{ServiceVariables.CUSTOM_HEAD_LABEL}</th>
-        <th>{ServiceVariables.CUSTOM_TAIL_LABEL}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td><center>{head_chance}%</center></td>
-        <td><center>{tail_chance}%</center></td>
-      </tr>
-    </tbody>
-    </table>
-    <br/>
-    <details>
-        <summary>Сбросить шансы можно тут</summary>
-        <br/>
-        <p>Нажатие на кнопку "Сбросить шансы" вернёт шансы к значениям 50 на 50.<br/>Не злоупотребляйте!</p>
-        <form action="{ServiceVariables.URL}/reset">
-          <input class="button" type="submit" value="Сбросить шансы" />
-        </form>
-        <br/>
-    </details>
-    <br/>
-    {history}
-    <br/>
-    <form action="{ServiceVariables.URL}/flip">
-      <input class="button" type="submit" value="Бросить монетку" />
-    </form>
-    <br/>
-    </center>
+    main_page_content = f"""
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Честная монетка</title>
+            <link id="favicon" rel="icon" type="image/x-icon" href="static/favicon.ico">
+        </head>
+        <style>
+        html {{
+          font-family: Arial, sans-serif;
+        }}
+        body {{
+          width: 800px;
+          margin: auto;
+        }}
+        .status-label {{
+          padding: 6pt;
+        }}
+        .button {{
+          background-color: #000000;
+          border: none;
+          color: white;
+          padding: 15px 32px;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          font-size: 16px;
+        }}
+        .table {{
+          width: 100%;
+          border: 2px solid;
+          padding: 6pt;
+        }}
+        td {{
+          height: 1.5em;
+          padding-left: 6pt;
+          padding-right: 6pt;
+          align: center;
+        }}
+        </style>
+        <body>
+            <!-- <h1>WebSocket Chat</h1>
+            <form action="" onsubmit="sendMessage(event)">
+                <input type="text" id="messageText" autocomplete="off"/>
+                <button>Send</button>
+            </form>
+            <ul id='messages'>
+            </ul> -->
+
+            <center>
+            <h1>Честная монетка</h1>
+            <h3>{quote}</h3>
+            <hr>
+            <p>Если тебе выпала не та сторона, которую ты хотел - шансы изменятся в твою пользу.</p>
+            <h1 id="waiting-text" class="status-label">Ожидание броска</b></h1>
+            <p><b>Текущие шансы</b></p>
+            <table class="table">
+            <thead>
+              <tr>
+                <th>{ServiceVariables.CUSTOM_HEAD_LABEL}</th>
+                <th>{ServiceVariables.CUSTOM_TAIL_LABEL}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><center>{head_chance}%</center></td>
+                <td><center>{tail_chance}%</center></td>
+              </tr>
+            </tbody>
+            </table>
+            <br/>
+            <details>
+                <summary>Сбросить шансы можно тут</summary>
+                <br/>
+                <p>Нажатие на кнопку "Сбросить шансы" вернёт шансы к значениям 50 на 50.<br/>Не злоупотребляйте!</p>
+                <form action="{ServiceVariables.URL}/reset">
+                  <input class="button" type="submit" value="Сбросить шансы" />
+                </form>
+                <br/>
+            </details>
+            <br/>
+            <section id="history_section">
+            {history}
+            </section>
+            <br/>
+            <form action="{ServiceVariables.URL}/flip">
+              <input class="button" type="submit" value="Бросить монетку" />
+            </form>
+            <br/>
+            </center>
+
+            <script>
+                var ws = new WebSocket("ws://{ServiceVariables.URL}/ws");
+                ws.onmessage = function(event) {{
+                    var messages = document.getElementById('messages')
+                    var message = document.createElement('li')
+                    var content = document.createTextNode(event.data)
+                    message.appendChild(content)
+                    messages.appendChild(message)
+                }};
+                function sendMessage(event) {{
+                    var input = document.getElementById("messageText")
+                    ws.send(input.value)
+                    input.value = ''
+                    event.preventDefault()
+                }};
+            </script>
+        </body>
+    </html>
     """
-    return get_core_page_template(inner=main_page)
+    return main_page_content
+    # return get_core_page_template(inner=main_page_content)
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import json
+from contextlib import asynccontextmanager
+from typing import List
+
 import uvicorn
-from fastapi import FastAPI, Header
-from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from content.result_page import get_result_page_content
+from starlette.websockets import WebSocketDisconnect
+
 from content.main_page import get_main_page_content
 from content.quotes import get_random_before_flip_quote, get_random_after_flip_quote
+from content.result_page import get_result_page_content
+from core.broadcast_sender import BroadcastSender
 from core.chances import get_chances, write_chances
 from core.flip import flip_coin
 from core.history import get_history, write_to_history
@@ -12,13 +19,20 @@ from core.vars import ServiceVariables
 from database.database import Database
 
 database = Database()
+broadcast_ws = BroadcastSender()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await broadcast_ws.generator.asend(None)
+    yield
 
 app = FastAPI(
     title="Fair Coin",
     description="Брось монетку и получи повышенные шансы выиграть, если проиграл",
     version="1.1.0",
     docs_url=None,
-    redoc_url=None
+    redoc_url=None,
+    lifespan=lifespan
 )
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
@@ -35,69 +49,116 @@ async def main_page():
         history=history
     )
 
-
-@app.get("/flip", response_class=RedirectResponse)
-async def flip():
-    chances = get_chances(db=database)
-
-    current_head_chance, current_tail_chance = chances.HEAD, chances.TAIL
-
-    flip_result = flip_coin(head_chance=current_head_chance, tail_chance=current_tail_chance)
-
-    write_to_history(
-        db=database,
-        result=flip_result.RESULT,
-        chances=f'{ServiceVariables.CUSTOM_HEAD_LABEL}: {current_head_chance}%, '
-                f'{ServiceVariables.CUSTOM_TAIL_LABEL}: {current_tail_chance}%'
-    )
-
-    write_chances(
-        db=database,
-        head_chance=flip_result.HEAD,
-        tail_chance=flip_result.TAIL
-    )
-
-    return RedirectResponse(
-        url=f'{ServiceVariables.URL}/result?side={flip_result.RESULT}'
-            f'&generation_pool={str(flip_result.GENERATION_POOL)}',
-        status_code=303
-    )
-
-
-@app.get("/result", response_class=HTMLResponse)
-async def result_page(
-        side="Вероятно, вы перешли сюда из истории, или изменили URL",
-        generation_pool="нет данных"
-):
-    chances = get_chances(db=database)
-
-    new_head_chance, new_tail_chance = chances.HEAD, chances.TAIL
-
-    history = get_history(db=database)
-    return get_result_page_content(
-        quote=get_random_after_flip_quote(),
-        side=side,
-        generation_pool=generation_pool,
-        head_chance=new_head_chance,
-        tail_chance=new_tail_chance,
-        history=history
-    )
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await broadcast_ws.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            if data == 'flip':
+                message = "coin_flip_response"
+                payload = {
+                    "new_head_chance": "int",
+                    "new_tail_chance": "int",
+                    "history": "history_HTML"
+                }
+            elif data == 'reset':
+                message = "chances_reset_response"
+                payload = {
+                    "new_head_chance": "int",
+                    "new_tail_chance": "int",
+                    "history": "history_HTML"
+                }
+            else:
+                message = "unknown_action"
+                payload = {}
+            await broadcast_ws.push(
+                json.dumps({
+                    "message": message,
+                    "payload": payload
+                })
+            )
+    except WebSocketDisconnect:
+        broadcast_ws.remove(websocket)
 
 
-@app.get("/reset")
-async def reset_chances():
-    write_chances(
-        db=database,
-        head_chance=50,
-        tail_chance=50
-    )
-    write_to_history(
-        db=database,
-        result="<b>Шансы сброшены</b>",
-        chances='<b>Шансы приведены к 50%</b>'
-    )
-    return RedirectResponse(ServiceVariables.URL)
+# @app.get("/push/{message}")
+# async def push_to_connected_websockets(message: str):
+#     await notifier.push(f"! Push notification: {message} !")
 
+# @app.get("/", response_class=HTMLResponse)
+# async def main_page():
+#     chances = get_chances(db=database)
+#     history = get_history(db=database)
+#     head_chance, tail_chance = chances.HEAD, chances.TAIL
+#     return get_main_page_content(
+#         quote=get_random_before_flip_quote(),
+#         head_chance=head_chance,
+#         tail_chance=tail_chance,
+#         history=history
+#     )
+
+# @app.get("/flip", response_class=RedirectResponse)
+# async def flip():
+#     chances = get_chances(db=database)
+#
+#     current_head_chance, current_tail_chance = chances.HEAD, chances.TAIL
+#
+#     flip_result = flip_coin(head_chance=current_head_chance, tail_chance=current_tail_chance)
+#
+#     write_to_history(
+#         db=database,
+#         result=flip_result.RESULT,
+#         chances=f'{ServiceVariables.CUSTOM_HEAD_LABEL}: {current_head_chance}%, '
+#                 f'{ServiceVariables.CUSTOM_TAIL_LABEL}: {current_tail_chance}%'
+#     )
+#
+#     write_chances(
+#         db=database,
+#         head_chance=flip_result.HEAD,
+#         tail_chance=flip_result.TAIL
+#     )
+#
+#     return RedirectResponse(
+#         url=f'{ServiceVariables.URL}/result?side={flip_result.RESULT}'
+#             f'&generation_pool={str(flip_result.GENERATION_POOL)}',
+#         status_code=303
+#     )
+
+
+# @app.get("/result", response_class=HTMLResponse)
+# async def result_page(
+#         side="Вероятно, вы перешли сюда из истории, или изменили URL",
+#         generation_pool="нет данных"
+# ):
+#     chances = get_chances(db=database)
+#
+#     new_head_chance, new_tail_chance = chances.HEAD, chances.TAIL
+#
+#     history = get_history(db=database)
+#     return get_result_page_content(
+#         quote=get_random_after_flip_quote(),
+#         side=side,
+#         generation_pool=generation_pool,
+#         head_chance=new_head_chance,
+#         tail_chance=new_tail_chance,
+#         history=history
+#     )
+
+
+# @app.get("/reset")
+# async def reset_chances():
+#     write_chances(
+#         db=database,
+#         head_chance=50,
+#         tail_chance=50
+#     )
+#     write_to_history(
+#         db=database,
+#         result="<b>Шансы сброшены</b>",
+#         chances='<b>Шансы приведены к 50%</b>'
+#     )
+#     return RedirectResponse(ServiceVariables.URL)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
